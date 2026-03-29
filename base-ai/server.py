40ae8ff39e31abf6a8ff793b3c489f7e3eab3a4b
Lukombo-JDS <dos-santos.lukombo@proton.me>
2026-03-29 07:38:14 +0200

    chore: adding routes

--- base-ai/server.py
+++ base-ai/server.py
@@ -6,3 +6,18 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/health")
+async def healthServer():
+    return {"message": "Ok everything good so far"}
+
+
+@app.post("/request")
+async def userRequests():
+    return {""}
+
+
+@app.get("/chats")
+async def listingChats():
+    return {""}
